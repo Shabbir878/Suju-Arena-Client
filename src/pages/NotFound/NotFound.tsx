567082ee8f94903b2c3f27248545ff71724134c5
Shabbir8788 <hasanalshabbir257@gmail.com>
2024-10-04 19:10:06 +0600

    Updated Client

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+import error_img from "../../assets/Error_blue.gif";
+
 const NotFound = () => {
   return (
-    <div className="py-[180px]">
-      <h1 className="text-center "> Page NotFound 404 !</h1>
+    <div className="flex justify-center flex-col items-center h-screen text-center">
+      <div className="w-1/2 md:w-1/3 lg:w-1/5">
+        <img src={error_img} alt="error image" />
+      </div>
+      <h2 className="text-6xl font-bold">404</h2>
+      <p className="text-2xl">Page is not found !</p>
+      <Link to="/" className="my-5">
+        {" "}
+        <button className="btn btn-info btn-rounded">Go to home</button>
+      </Link>
     </div>
   );
 };
